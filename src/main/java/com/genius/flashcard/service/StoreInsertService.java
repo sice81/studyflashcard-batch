c914b6cce8141d819bc392e4b11240dc42b3cde2
sice81 <jaeikpark81@gmail.com>
2015-08-17 23:46:25 +0900

    스토어 순위 구하는 쿼리 수정

--- src/main/java/com/genius/flashcard/service/StoreInsertService.java
+++ src/main/java/com/genius/flashcard/service/StoreInsertService.java
@@ -22,16 +22,16 @@ public class StoreInsertService {
 		logger.info("insertFrom()");
 		StringBuffer sb = new StringBuffer();
 
-		sb.append("INSERT INTO CARDPACK_STORE                                                                                            " );
-		sb.append("WITH WT1 AS (                                                                                                         " );
-		sb.append("  SELECT CARDPACK_ID, SUM(BACK_VIEW_CNT) + SUM(RIGHT_CNT)*2 + SUM(WRONG_CNT) IDX FROM STUDY_ACT_LOG                 " );
-		sb.append("  GROUP BY CARDPACK_ID                                                                                                " );
-		sb.append("  ORDER BY 2 DESC                                                                                                     " );
-		sb.append("), WT2 AS (                                                                                                           " );
-		sb.append("  SELECT RANK() OVER (ORDER BY A.IDX DESC) RANK, A.IDX, A.CARDPACK_ID, B.CARDPACK_NAME TITLE FROM WT1 A, CARDPACKS B" );
-		sb.append("  WHERE A.CARDPACK_ID = B.CARDPACK_ID                                                                                 " );
-		sb.append(")                                                                                                                     " );
-		sb.append("SELECT RANK, CARDPACK_ID, TITLE FROM WT2                                                                               ");
+		sb.append("INSERT INTO CARDPACK_STORE \n" );
+		sb.append("WITH WT1 AS ( \n" );
+		sb.append("  SELECT B.CARDPACK_ID, NVL(SUM(A.BACK_VIEW_CNT) + SUM(A.RIGHT_CNT)*2 + SUM(A.WRONG_CNT), 0) IDX FROM CARDPACKS B, STUDY_ACT_LOG A \n" );
+		sb.append("  WHERE B.CARDPACK_ID = A.CARDPACK_ID(+) \n" );
+		sb.append("  GROUP BY B.CARDPACK_ID \n" );
+		sb.append("), WT2 AS ( \n" );
+		sb.append("  SELECT ROW_NUMBER() OVER (ORDER BY A.IDX DESC) RANK, A.IDX, A.CARDPACK_ID, B.CARDPACK_NAME TITLE FROM WT1 A, CARDPACKS B \n" );
+		sb.append("  WHERE A.CARDPACK_ID = B.CARDPACK_ID \n" );
+		sb.append(") \n" );
+		sb.append("SELECT RANK, CARDPACK_ID, TITLE FROM WT2");
 
 		jdbcTemplate.update(sb.toString());
 	}
